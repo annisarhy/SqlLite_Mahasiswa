0eb27d8c96eb8e1080ec00c9c20450d7fa37b78a
Annisarhy <analisarahayu@gmail.com>
2019-09-14 08:47:04 +0700

    DATABASE

---
+++ app/src/main/java/com/example/sqllite_mahasiswa/DatabaseHelper.java
@@ -0,0 +1,24 @@
+package com.example.sqllite_mahasiswa;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "biodata_mahasiswa.db";
+    private static final int DATABASE_VERSION =1;
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME,null,DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String sql = "create table biodata(telpon integer primary key, nama text null, tgl text null, jk text null, alamat text null);";
+        Log.d("Data","onCreate :"+sql);
+        db.execSQL(sql);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2){
+        
+    }
+}
