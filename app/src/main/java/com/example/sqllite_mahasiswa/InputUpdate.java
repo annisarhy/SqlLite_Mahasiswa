700865137beef9bf8f26fe814e752f93d2d32310
Annisarhy <analisarahayu@gmail.com>
2019-09-14 12:41:14 +0700

    DATABASE

--- app/src/main/java/com/example/sqllite_mahasiswa/InputUpdate.java
+++ app/src/main/java/com/example/sqllite_mahasiswa/InputUpdate.java
@@ -2,21 +2,29 @@ package com.example.sqllite_mahasiswa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class InputUpdate extends AppCompatActivity {
-    EditText edtTextNomor,edtTextNama,edtTextTglLahir,edtTextJenkel,edtTextAlamat;
-    Button btn_simpan;
-    DatabaseHelper SQLite = new DatabaseHelper(this);
-    String nama,tgl_lahir,nomor,jenkel,alamat;
+    EditText edtTextNomor, edtTextNama, edtTextTglLahir, edtTextJenkel, edtTextAlamat;
+    Context context;
+    Button btnSimpan;
+    DatabaseHelper editdata;
+    String nama, tgl_lahir, nomor, jenkel, alamat, submit;
+    PersonBean update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_input_update);
+        context = this;
 
         edtTextNomor = findViewById(R.id.edtTextNomor);
         edtTextNama = findViewById(R.id.edtTextNama);
@@ -24,6 +32,53 @@ public class InputUpdate extends AppCompatActivity {
         edtTextTglLahir = findViewById(R.id.edtTextTglLahir);
         edtTextAlamat = findViewById(R.id.edtTextAlamat);
 
-        nomor = getIntent().getStringExtra(DataMahasiswaActivity.KEY_NAMA);
+        submit = getIntent().getStringExtra("UPDATE_ACTION");
+        update = getIntent().getParcelableExtra("UPDATE_INTENT");
+        if (submit == null) {
+            submit = "Submit";
+        } else {
+            nomor = String.valueOf(update.getNomor());
+        }
+
+        if (submit.equals("Update")){
+            btnSimpan.setText("Update");
+            edtTextNomor.setText(nomor);
+            edtTextNomor.setFocusable(false);
+            edtTextNama.setText(update.getNama());
+            edtTextTglLahir.setText(update.getTgl_lahir());
+            edtTextJenkel.setText(update.getJenkel());
+            edtTextAlamat.setText(update.getAlamat());
+
+            Log.d("test",update.getNama());
+        }
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseHelper db = new DatabaseHelper(context);
+                PersonBean data = new PersonBean();
+                String label = btnSimpan.getText().toString();
+                if (label.equals("Simpan")){
+                    data.setNomor(Integer.parseInt(edtTextNomor.getText().toString()));
+                    data.setNama(edtTextNama.getText().toString());
+                    data.setTgl_lahir(edtTextTglLahir.getText().toString());
+                    data.setJenkel(edtTextJenkel.getText().toString());
+                    data.setAlamat(edtTextAlamat.getText().toString());
+                    db.insert(data);
+                    Intent move = new Intent(context,MainActivity.class);
+                    context.startActivity(move);
+                }
+                if (label.equals("Update")){
+                    data.setNomor(Integer.parseInt(edtTextNomor.getText().toString()));
+                    data.setNama(edtTextNama.getText().toString());
+                    data.setTgl_lahir(edtTextTglLahir.getText().toString());
+                    data.setJenkel(edtTextJenkel.getText().toString());
+                    data.setAlamat(edtTextAlamat.getText().toString());
+                    db.update(data);
+                    Intent move = new Intent(context,MainActivity.class);
+                    context.startActivity(move);
+                }
+            }
+        });
+
     }
 }
